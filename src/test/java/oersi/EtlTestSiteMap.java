f17703b7c2202b3b01089cd669cce292f7f723e0
Fabian Steeg <steeg@hbz-nrw.de>
2021-02-19 11:27:33 +0100

    Switch EtlTestSiteMap to local test resources
    See https://gitlab.com/oersi/oersi-etl/-/issues/58

--- src/test/java/oersi/EtlTestSiteMap.java
+++ src/test/java/oersi/EtlTestSiteMap.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -22,11 +21,11 @@ import org.xml.sax.SAXException;
 public class EtlTestSiteMap {
 
     private static final Object[][] PARAMS = new Object[][] { //
-            { "https://www.hoou.de/sitemap.xml", "https://www.hoou.de/materials/", Arrays.asList( // -->
-                    "https://www.hoou.de/materials/tutorial-lernen-lernen", //
-                    "https://www.hoou.de/materials/online-punkteabfrage", //
-                    "https://www.hoou.de/materials/ifm-erzahlung-wie") }, //
-            { "https://www.oncampus.de/sitemap.xml", "https://www.oncampus.de/", Arrays.asList( // -->
+            { "/hoou-sitemap.xml", "https://www.hoou.de/materials/", Arrays.asList( // -->
+                    "https://www.hoou.de/materials/forschungsfrage-finden-trickfilme-zum-prozess", //
+                    "https://www.hoou.de/materials/die-trompete-als-orchester-und-soloinstrument", //
+                    "https://www.hoou.de/materials/stadt-als-sozialer-raum") }, //
+            { "/oncampus-sitemap.xml", "https://www.oncampus.de/", Arrays.asList( // -->
                     "https://www.oncampus.de/Customer_Experience_Management", //
                     "https://www.oncampus.de/Leben+in+Deutschland", //
                     "https://www.oncampus.de/Propädeutik_Mathe_Grundlagen") } //
@@ -37,25 +36,26 @@ public class EtlTestSiteMap {
         return Arrays.asList(PARAMS);
     }
 
-    private String siteMapUrl;
+    private String sitemap;
     private String prefix;
     private List<String> urls;
 
-    public EtlTestSiteMap(String siteMapUrl, String prefix, List<String> titles) {
-        this.siteMapUrl = siteMapUrl;
+    public EtlTestSiteMap(String sitemap, String prefix, List<String> titles) {
+        this.sitemap = sitemap;
         this.prefix = prefix;
         this.urls = titles;
     }
 
     @Test
     public void testConvertSitemap() throws IOException, SAXException {
-        List<String> resultUrls = readSiteMap(siteMapUrl, prefix);
+        List<String> resultUrls = readSiteMap(sitemap, prefix);
         assertFalse("Result URLs should not be empty", resultUrls.isEmpty());
         urls.forEach(url -> assertThat(resultUrls, hasItem(url)));
     }
 
-    private List<String> readSiteMap(String sitemapUrl, String prefix) throws SAXException, IOException {
-        Match siteMapXml = JOOX.$(new URL(sitemapUrl).openStream());
+    private List<String> readSiteMap(String sitemap, String prefix)
+            throws SAXException, IOException {
+        Match siteMapXml = JOOX.$(getClass().getResourceAsStream(sitemap));
         return siteMapXml.find("loc").filter(matchText("^" + prefix + ".*")).texts();
     }
 
