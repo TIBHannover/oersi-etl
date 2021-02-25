79e18defd21ee87ee24feec65356351eb654fc00
Fabian Steeg <steeg@hbz-nrw.de>
2021-02-25 12:42:56 +0100

    Move sitemap integration tests to unit tests
    See https://gitlab.com/oersi/oersi-etl/-/issues/58

--- src/test/java/oersi/TestSitemapReader.java
+++ src/test/java/oersi/TestSitemapReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Fabian Steeg, hbz
+ * Copyright 2020, 2021 Fabian Steeg, hbz
  *
  * Licensed under the Apache License, Version 2.0 the "License";
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,15 @@
  */
 package oersi;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.metafacture.framework.MetafactureException;
 import org.metafacture.framework.ObjectReceiver;
 import org.mockito.InOrder;
@@ -31,12 +37,38 @@ import org.mockito.MockitoAnnotations;
  * @author Fabian Steeg
  *
  */
+@RunWith(Parameterized.class)
 public final class TestSitemapReader {
 
-    private static final String SITEMAP_URL = "https://www.oerbw.de/edu-sharing/eduservlet/sitemap?from=0";
-    private static final String BASE = "https://uni-tuebingen.oerbw.de/edu-sharing/components/render/";
-    private static final String ON_PAGE_1 = BASE + "651396b4-db24-44e4-a0f6-76aff3293f90"; // from=0
-    private static final String ON_PAGE_2 = BASE + "605a7ed8-d318-4a2c-8c25-2a5985c85e15"; // from=500
+    private static final Object[][] PARAMS = new Object[][] { //
+            { "/hoou-sitemap.xml", "https://www.hoou.de/materials/", Arrays.asList( // -->
+                    "https://www.hoou.de/materials/forschungsfrage-finden-trickfilme-zum-prozess", //
+                    "https://www.hoou.de/materials/die-trompete-als-orchester-und-soloinstrument", //
+                    "https://www.hoou.de/materials/stadt-als-sozialer-raum") }, //
+            { "/oncampus-sitemap.xml", "https://www.oncampus.de/", Arrays.asList( // -->
+                    "https://www.oncampus.de/Customer_Experience_Management", //
+                    "https://www.oncampus.de/Leben+in+Deutschland", //
+                    "https://www.oncampus.de/Propädeutik_Mathe_Grundlagen") }, //
+            { "/oerbw-sitemap.xml", "https://uni-tuebingen.oerbw.de/", Arrays.asList( // -->
+                    "https://uni-tuebingen.oerbw.de/edu-sharing/components/render/907ca040-2d0c-4a89-a122-177d02df7685", //
+                    "https://uni-tuebingen.oerbw.de/edu-sharing/components/render/76bd5ef6-8607-4294-8a8c-2494aeff5ee9", //
+                    "https://uni-tuebingen.oerbw.de/edu-sharing/components/render/e65bb0f8-c30d-4205-9dcf-a99d372c2012") } //
+    };
+
+    @Parameterized.Parameters(name = "{0}, {1} -> {2}")
+    public static Collection<Object[]> siteMaps() {
+        return Arrays.asList(PARAMS);
+    }
+
+    private String sitemap;
+    private String prefix;
+    private List<String> urls;
+
+    public TestSitemapReader(String sitemap, String prefix, List<String> titles) {
+        this.sitemap = sitemap;
+        this.prefix = prefix;
+        this.urls = titles;
+    }
 
     private SitemapReader sitemapReader;
 
@@ -50,20 +82,17 @@ public final class TestSitemapReader {
         sitemapReader = new SitemapReader();
         sitemapReader.setLimit(-1);
         sitemapReader.setWait(0);
+        sitemapReader.setUrlPattern("^" + prefix + ".*");
         sitemapReader.setReceiver(receiver);
         inOrder = Mockito.inOrder(receiver);
     }
 
     @Test
-    public void testShouldProcessPage1() {
-        sitemapReader.process(SITEMAP_URL);
-        inOrder.verify(receiver, Mockito.calls(1)).process(ON_PAGE_1);
-    }
-
-    @Test
-    public void testShouldProcessPage2() {
-        sitemapReader.process(SITEMAP_URL);
-        inOrder.verify(receiver, Mockito.calls(1)).process(ON_PAGE_2);
+    public void testShouldProcess() {
+        sitemapReader.process(getClass().getResource(sitemap).toString());
+        for (String url : urls) {
+            inOrder.verify(receiver, Mockito.calls(1)).process(url);
+        }
     }
 
     @Test(expected = MetafactureException.class)
